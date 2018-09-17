8a920153d5ec85e8b62db4e4a491291c67efd352
Andreas Martin <andreas.martin@onlinehome.de>
2018-09-17 21:44:56 +0200

    HP weitergebaut

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,45 @@ class HomeController extends AbstractController
     {
         return $this->render('home/home.html.twig');
     }
+
+     /**
+     *@Route("/ug",name="ug")
+     */
+    public function kurz()
+    {
+        return $this->render('Kurz_Lang_Uebergroessen/klu.html.twig');
+    }
+
+    /**
+     *@Route("/aenderung",name="aenderung")
+     */
+    public function aenderung()
+    {
+        return $this->render('aenderung/aenderung.html.twig');
+    }
+
+    /**
+     *@Route("/stoffverkauf",name="stoffverkauf")
+     */
+    public function stoffverkauf()
+    {
+        return $this->render('stoffverkauf/stoff.html.twig');
+    }
+
+    /**
+     *@Route("/cafe",name="cafe")
+     */
+    public function cafe()
+    {
+        return $this->render('cafe/cafe.html.twig');
+    }
+
+    /**
+     *@Route("/restposten",name="restposten")
+     */
+    public function restpsoten()
+    {
+        return $this->render('restposten/restposten.html.twig');
+    }
+
 }
